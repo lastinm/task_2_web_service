832372f263d571c868c35711c459d66732aa6ec4
Maxim Lastin <lastinm@mail.ru>
2023-11-10 23:55:12 +0300

    Added script on python and screeshot

--- task_2_web_service.py
+++ task_2_web_service.py
@@ -1,27 +1,24 @@
-#import io
 import re
 import streamlit as st
-#from PIL import Image
-#import numpy as np
-#from tensorflow.keras.applications import EfficientNetB0
-#from tensorflow.keras.preprocessing import image
-#from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 from transformers import pipeline
 
-@st.cache(allow_output_mutation=True)
-def load_model(text_to_fill):
-    unmasker = pipeline('fill-mask', model='xlm-roberta-base')
-    return unmasker(text_to_fill)
+# Декоратор @st.cache говорит Streamlit, что модель нужно загрузить только один раз, чтобы избежать утечек памяти
+@st.cache_data
+def load_model():
+    return pipeline('fill-mask', model='xlm-roberta-base')
+
+# Загружаем предварительно обученную модель
+model = load_model()
 
 st.title('Подбор подходящих определений вместо шаблона')
-st.echo(code_location=None)
 
-#with st.echo("below"):
-#with st.spinner(text="Думаем..."):
 answer = st.text_input("Пожалуйста, напишите предложение, указав вместо подбираемого слова шаблон <mask>:")
-match = re.search("<mask>", answer)
-if match:
-    result = load_model(text_to_fill = answer)
-    st.write(result)
+all_ok = re.search("<mask>", answer)
+
+if all_ok:
+    result = model(answer)
+    for i, sting in enumerate(result):
+        st.write("Вариант ", i, ": ", sting['sequence'])
+
 
 
